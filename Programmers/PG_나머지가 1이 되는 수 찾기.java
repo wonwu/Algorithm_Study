adbeeb9a199d835c594fa69711140ec6d3eb295f
Jiwon Lim <57473228+wonwu@users.noreply.github.com>
2022-05-13 23:55:57 +0900

    Create PG_나머지가 1이 되는 수 찾기.java

---
+++ Programmers/PG_나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,15 @@
+// PG_나머지가 1이 되는 수 찾기
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2; i <= n-1; i++){
+            if(n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
